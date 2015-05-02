e260298d8258f268adccee919fbc8ff68f90f91b
Foxracing <phu6gt@gmail.com>
2015-05-02 14:17:34 -0400

    roundcontrol updated

--- roundcontrol.py
+++ roundcontrol.py
@@ -15,51 +15,73 @@ TOTAL_ROUND = int(4)
 
 class player:
     #betting = ["fold","call","raise"]
-    def __init__(self, hands, initalMoney = 100):
-        self.hands = list(hands)
+    def __init__(self, isComputer, holeCards, initalMoney = 500):
+        self.holeCards = list(holeCards)
         self.betHistory = []
         self.betMoney = []
         self.moneyInHand = int(initalMoney)
+        self.potMoney = 0
+        self.lastBet = 0
+        self.isComputer = isComputer
+        
 
     def holdcards(self):
-        cards = list(self.hands)
+        cards = list(self.holeCards)
         return cards
 
     def bet(self, newbet):
         self.betMoney.append(int(newbet))
         self.moneyInHand -= int(newbet)
+        self.potMoney += int(newbet)
+        self.lastBet = newbet
         return self.moneyInHand
 
-    def currentMoney(self):
-        return self.moneyInHand
-
-    def resetCards(self, hands):
-        self.hands = list(hands)
+    def resetCards(self, holeCards):
+        self.holeCards = list(holeCards)
+        self.potMoney = 0
+        self.lastBet = 0
 
 class roundcontrol:
-    def __init__(self, players):
-        self.numply = players
-        pubcards, plycards = dc.texassim(players) #deal
-        self.pub = pubcards #board cards
-        self.plys = [] #players
+    def __init__(self, numPlayer = 2):
+        self.numPlayer = numPlayer
+        pubcards, plycards = dc.texassim(self.numPlayer) #deal
+        self.publicCards = pubcards #board cards
+        self.playerList = []
+        
+        isComputer = False
         for onehand in plycards:
-            self.plys.append( player(onehand) )
+            self.playerList.append(player(isComputer, onehand))
+            isComputer = True
+
+    def resetGame(self):
+        pubcards, plycards = dc.texassim(self.numPlayer) #deal
+        self.publicCards = pubcards
+        i = 0
+        for onehand in plycards:
+            self.playerList[i].resetCards(onehand)
+            i += 1
 
     def boardcard(self, round=TOTAL_ROUND-1):
         if round == 0:
             cards = []
         else:
-            cards = list( self.pub[:2+round] )
+            cards = list( self.publicCards[:2+round] )
         return cards
 
     def player(self, no):
-        return self.plys[no]
+        return self.playerList[no]
+
+    def currentmoneyinpot(self):
+        total = 0
+        for player in self.playerList:
+            total += player.potMoney
+        return total
 
 
 def main():
     numplayers = 2
     playerID = np.random.randint(numplayers)
-    dealer = roundcontrol(numplayers)
+    dealer = roundcontrol()
     for curound in xrange(TOTAL_ROUND):
         print "===============round", curound, "======================="
         print "The current board cards are:"
