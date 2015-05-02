0f346797c9eb613584f2163db624219b2d2f912a
Yj Cheng <cheng.yungjen@gmail.com>
2015-05-02 03:28:47 -0400

    added winningprob

--- roundcontrol.py
+++ roundcontrol.py
@@ -10,8 +10,10 @@
 #-------------------------------------------------------------------------------
 import deal_compare as dc
 import numpy as np
+import pickle
+import initial_52
 
-TOTAL_ROUND = int(4)
+TOTAL_TURN = int(4)
 
 class player:
     #betting = ["fold","call","raise"]
@@ -38,7 +40,7 @@ class roundcontrol:
         for onehand in plycards:
             self.plys.append( player(onehand) )
 
-    def boardcard(self, round=TOTAL_ROUND-1):
+    def boardcard(self, round=TOTAL_TURN-1):
         if round == 0:
             cards = []
         else:
@@ -48,6 +50,13 @@ class roundcontrol:
     def player(self, no):
         return self.plys[no]
 
+# constants
+allCards = initial_52.cards_vec()
+card2n = dict(zip(allCards, np.arange(52)))
+n2card = dict(zip(np.arange(52), allCards))
+
+Comparedresult= pickle.load(open('Comparedresult.pcl', 'r'))
+rankresult= pickle.load(open('rankresult.pcl', 'r'))
 
 def main():
     numplayers = 2
---
+++ winningprob.py
@@ -0,0 +1,117 @@
+#-------------------------------------------------------------------------------
+# Name:        winningprob.py
+# Purpose:
+#
+# Author:      Yj
+#
+# Created:     01/05/2015
+# Copyright:   (c) Yj 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import deal_compare as dc
+import each_compare as ec
+import numpy as np
+import itertools
+import pandas as pd
+import pickle
+import initial_52
+
+# find out the best five cards from the seven cards
+def bestfive(sevencards):
+    '''
+    sevencards: list of seven cards. eg. ['6D', '7H', '2C', 'AD', 'AH', '2D', 'TC']
+    return: best 5 cards, strength of the best 5 cards
+    '''
+    # list all combinations 5 out of 7
+    permu = list(itertools.combinations(sevencards, 5))
+    lind = 0
+    lstr = ALLCOMP["".join(sorted(permu[lind]))]
+    for i,five in enumerate(permu):
+        key = "".join(sorted(five))
+        cstr = ALLCOMP[key]
+        if cstr > lstr:
+            lind = i
+            lstr = cstr
+    return list(permu[lind]), lstr
+
+def possiblecards(exclude, allcards=ALLCARDS):
+    return [ c for c in allcards if c not in exclude ]
+
+#==============================================================================#
+
+def winc_river(boardcards, holecards, exclude=[]):
+    assert( len(boardcards) == 5 ) #river has five boardcards
+    myhighfive, mystrength = bestfive(boardcards + holecards)
+    # the rest possible cards of opponent's hole cards
+    resetcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
+    opponentholecards = itertools.combinations(resetcards, 2)
+    wincount, tiecount, totalcount = 0,0,0
+    for oneholecard in opponentholecards:
+        totalcount += 1
+        hishighfive, hisstrength = bestfive(boardcards + list(oneholecard))
+        if mystrength > hisstrength:
+            wincount += 1
+        elif mystrength == hisstrength:
+            tiecount += 1
+    return wincount, tiecount, totalcount
+
+def winp_river(boardcards, holecards, exclude=[]):
+    wincount, tiecount, totalcount= winc_river(boardcards, holecards, exclude)
+    return ( wincount + 0.5*tiecount ) / totalcount
+
+#==============================================================================#
+
+def winc_turn(boardcards, holecards, exclude=[]):
+    assert( len(boardcards) == 4 ) #turn has four boardcards
+    # the rest possible cards of one board card and opponent's hole cards
+    resetcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
+    restboardcard = itertools.combinations(resetcards, 1)
+    wincount, tiecount, totalcount = 0,0,0
+    for oneboardcard in restboardcard:
+        subwincount, subtiecount, subtotalcount = winc_river( boardcards+list(oneboardcard), holecards )
+        wincount += subwincount
+        tiecount += subtiecount
+        totalcount += subtotalcount
+    return wincount, tiecount, totalcount
+
+def winp_turn(boardcards, holecards, exclude=[]):
+    wincount, tiecount, totalcount= winc_turn(boardcards, holecards, exclude)
+    return ( wincount + 0.5*tiecount ) / totalcount
+
+#==============================================================================#
+
+def winc_flop(boardcards, holecards, exclude=[]):
+    assert( len(boardcards) == 3 ) #flop has three boardcards
+    # the rest possible cards of one board card and opponent's hole cards
+    resetcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
+    restboardcard = itertools.combinations(resetcards, 1)
+    wincount, tiecount, totalcount = 0,0,0
+    for oneboardcard in restboardcard:
+        subwincount, subtiecount, subtotalcount = winc_turn( boardcards+list(oneboardcard), holecards )
+        wincount += subwincount
+        tiecount += subtiecount
+        totalcount += subtotalcount
+    return wincount, tiecount, totalcount
+
+def winp_flop(boardcards, holecards, exclude=[]):
+    wincount, tiecount, totalcount= winc_flop(boardcards, holecards, exclude)
+    return ( wincount + 0.5*tiecount ) / totalcount
+
+#==============================================================================#
+
+def initialize():
+    # initialize ALLCOMP
+    global ALLCOMP
+    filehandler = open("allCombDict_new.p","rb")
+    ALLCOMP = pickle.load(filehandler)
+    filehandler.close()
+    # initialize ALLCARDS
+    global ALLCARDS
+    ALLCARDS = initial_52.cards_vec()
+
+if __name__ == 'winningprob':
+   initialize()
+
+
+
+
