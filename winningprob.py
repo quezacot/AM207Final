78e511bccb97589cd1d634f352f2a181c4a51936
Yj Cheng <cheng.yungjen@gmail.com>
2015-05-02 23:40:48 -0400

    fix winningprob

--- winningprob.py
+++ winningprob.py
@@ -16,8 +16,8 @@ import pandas as pd
 import pickle
 import initial_52
 
-ALLCOMP = None
-ALLCARDS = None
+ALLCOMP
+ALLCARDS
 
 # find out the best five cards from the seven cards
 def bestfive(sevencards):
@@ -46,8 +46,8 @@ def winc_river(boardcards, holecards, exclude=[]):
     assert( len(boardcards) == 5 ) #river has five boardcards
     myhighfive, mystrength = bestfive(boardcards + holecards)
     # the rest possible cards of opponent's hole cards
-    resetcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
-    opponentholecards = itertools.combinations(resetcards, 2)
+    restcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
+    opponentholecards = itertools.combinations(restcards, 2)
     wincount, tiecount, totalcount = 0,0,0
     for oneholecard in opponentholecards:
         totalcount += 1
@@ -67,8 +67,8 @@ def winp_river(boardcards, holecards, exclude=[]):
 def winc_turn(boardcards, holecards, exclude=[]):
     assert( len(boardcards) == 4 ) #turn has four boardcards
     # the rest possible cards of one board card and opponent's hole cards
-    resetcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
-    restboardcard = itertools.combinations(resetcards, 1)
+    restcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
+    restboardcard = itertools.combinations(restcards, 1)
     wincount, tiecount, totalcount = 0,0,0
     for oneboardcard in restboardcard:
         subwincount, subtiecount, subtotalcount = winc_river( boardcards+list(oneboardcard), holecards )
@@ -86,8 +86,8 @@ def winp_turn(boardcards, holecards, exclude=[]):
 def winc_flop(boardcards, holecards, exclude=[]):
     assert( len(boardcards) == 3 ) #flop has three boardcards
     # the rest possible cards of one board card and opponent's hole cards
-    resetcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
-    restboardcard = itertools.combinations(resetcards, 1)
+    restcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
+    restboardcard = itertools.combinations(restcards, 1)
     wincount, tiecount, totalcount = 0,0,0
     for oneboardcard in restboardcard:
         subwincount, subtiecount, subtotalcount = winc_turn( boardcards+list(oneboardcard), holecards )
@@ -99,6 +99,21 @@ def winc_flop(boardcards, holecards, exclude=[]):
 def winp_flop(boardcards, holecards, exclude=[]):
     wincount, tiecount, totalcount= winc_flop(boardcards, holecards, exclude)
     return ( wincount + 0.5*tiecount ) / totalcount
+
+#==============================================================================#
+
+def opp_winp_river(boardcards, holecards, exclude=[]):
+    assert( len(boardcards) == 5 ) #river has five boardcards
+    # the rest possible cards of opponent's hole cards
+    restcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
+    opponentholecards = itertools.combinations(restcards, 2)
+    opp_winp = {}
+    for oneholecard in opponentholecards:
+        holecardlist = list(oneholecard)
+        winrate = winp_river(boardcards, holecardlist)
+        keyofholecard = "".join( sorted(holecardlist) )
+        opp_winp[keyofholecard] = winrate
+    return opp_winp
 
 #==============================================================================#
 
