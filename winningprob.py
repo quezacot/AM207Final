37275516d4dd41c35c4f38cb2be93eb1769f2c1f
Yj Cheng <cheng.yungjen@gmail.com>
2015-05-03 16:11:29 -0400

    flop appx by table

--- winningprob.py
+++ winningprob.py
@@ -16,8 +16,11 @@ import pandas as pd
 import pickle
 import initial_52
 
-ALLCOMP
-ALLCARDS
+ALLCOMP = None
+ALLCARDS = None
+TYPEWIN = None
+TYPELIST = [u'straightflush', u'fourkind', u'fullhouse', u'flush',
+            u'straight', u'threekind', u'twopairs', u'onepair', u'highcard']
 
 # find out the best five cards from the seven cards
 def bestfive(sevencards):
@@ -39,6 +42,15 @@ def bestfive(sevencards):
 
 def possiblecards(exclude, allcards=ALLCARDS):
     return [ c for c in allcards if c not in exclude ]
+
+def determintype(fivecards):
+    funclist = [dc.straightflush, dc.fourkind, dc.fullhouse, dc.flush, dc.straight, dc.threekind, dc.twopairs, dc.onepair]
+    for i in xrange(len(funclist)):
+        istype = funclist[i](fivecards)
+        if istype[-1]:
+            return TYPELIST[i]
+    return TYPELIST[-1]
+
 
 #==============================================================================#
 
@@ -100,6 +112,24 @@ def winp_flop(boardcards, holecards, exclude=[]):
     wincount, tiecount, totalcount= winc_flop(boardcards, holecards, exclude)
     return ( wincount + 0.5*tiecount ) / totalcount
 
+def winp_flop_appx(boardcards, holecards, exclude=[]):
+    assert( len(boardcards) == 3 ) #flop has three boardcards
+    # the rest possible cards of opponent's hole cards
+    restcards = possiblecards(boardcards + holecards + exclude, ALLCARDS)
+    restboardcards = itertools.combinations(restcards, 2)
+    typecount = dict(zip(TYPELIST, [0]*len(TYPELIST)))
+    for twoboardcards in restboardcards:
+        myhighfive, mystrength = bestfive(boardcards + list(twoboardcards))
+        myhighfivetype = determintype(myhighfive)
+        typecount[myhighfivetype] += 1
+    weightedsum = float(0)
+    totalcount = int(0)
+    for onetype in TYPELIST:
+        totalcount += typecount[onetype]
+        weightedsum += typecount[onetype] * TYPEWIN[onetype]['P']
+    return weightedsum / totalcount
+
+
 #==============================================================================#
 
 def opp_winp_river(boardcards, holecards, exclude=[]):
@@ -126,6 +156,11 @@ def initialize():
     # initialize ALLCARDS
     global ALLCARDS
     ALLCARDS = initial_52.cards_vec()
+    # initialize TYPEWIN
+    global TYPEWIN
+    filehandler = open("TypeWin.pkl","rb")
+    TYPEWIN = pickle.load(filehandler)
+    filehandler.close()
 
 if __name__ == 'winningprob':
    initialize()
